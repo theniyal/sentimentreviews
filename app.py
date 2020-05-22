eb57ed601023f259ec86fff3946b1cd4cccf0c58
Daniyal Siddiqui <daniyal3siddiqui@gmail.com>
2020-05-22 08:42:29 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -19,24 +19,24 @@ model = pickle.load(open('model.pkl', 'rb'))
 
 app = Flask(__name__)
 
-@app.route('/',  methods=['POST', 'GET'])
+@app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/predict', methods=['POST', 'GET'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    if request.method == "POST":
+    if request.method == 'POST':
         message = request.form['message']
-        # review = []
+        review = []
         data = [message]
-        # ps=PorterStemmer()
-        # sent = re.sub('[^A-Za-z]', ' ', message).lower().split()
-        # sent = [ps.stem(i) for i in sent if not i in stopwords.words('english')]
-        # sent = ' '.join(sent)
-        # review.append(sent)
-        review = cv.transform(data).toarray()
+        ps=PorterStemmer()
+        sent = re.sub('[^A-Za-z]', ' ', data).lower().split()
+        sent = [ps.stem(i) for i in sent if not i in stopwords.words('english')]
+        sent = ' '.join(sent)
+        review.append(sent)
+        review = cv.transform(review).toarray()
         my_prediction = model.predict(review)
-    return render_template('result.html', prediction=my_prediction)
+    return render_template('result.html', prediction = my_prediction)
 
 if __name__ == '__main__':
     app.debug = True
